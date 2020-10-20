bda53e0202fd26570880532a1cfbf7d70dfbfe55
John Allison <john123allison@gmail.com>
2020-10-20 14:58:07 -0400

    docstrings

--- app/models.py
+++ app/models.py
@@ -4,7 +4,12 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
+    """
+    This class models a User for the application, with methods to set a password, check it,
+    and return a string representation. Data for runs that are Run objects.
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -20,7 +25,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
 class Run(db.Model):
+    """
+    This class models a Run, with an id, timestamp, distance, and associated user. 
+    """
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     distance_miles = db.Column(db.Float)
@@ -35,6 +44,8 @@ class Run(db.Model):
         return self.timestamp < other.timestamp
 
 # load user id from database for flask-login to use
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
